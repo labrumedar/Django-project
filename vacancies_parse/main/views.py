7fb607fd36dd396f635ddb37a1ad03e027da887e
Meteleva Daria <meteleva797@ya.ru>
2024-12-28 00:03:06 +0500

    html/css for main

--- vacancies_parse/main/views.py
+++ vacancies_parse/main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h2>привет</h2>")
+    return render(request, 'main/index.html')
