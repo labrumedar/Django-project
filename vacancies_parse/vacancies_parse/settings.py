b967e8b49491d0354fa76682db908f36108e528b
Meteleva Daria <meteleva797@ya.ru>
2025-01-17 00:36:41 +0500

    update settings

--- vacancies_parse/vacancies_parse/settings.py
+++ vacancies_parse/vacancies_parse/settings.py
@@ -129,3 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "vacancies_parse", "main", "static"),
+]
